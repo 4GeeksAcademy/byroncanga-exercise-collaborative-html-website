8b653eb0bc22f92915601a46f72b95e9d30a8347
Lina María <66230572+linamaria126@users.noreply.github.com>
2023-11-02 01:47:41 +0000

    Services

--- website/assets/js/fix.js
+++ website/assets/js/fix.js
@@ -58,15 +58,6 @@ for (var i = 0; i < dropdowns.length; i++) {
     });
 }
 
-// Close dropdowns when screen becomes big enough to switch to open by hover
-function closeMenusOnResize() {
-    if (document.body.clientWidth >= 768) {
-        closeMenus();
-        collapse.classList.add('collapse');
-        collapse.classList.remove('in');
-    }
-}
-
 // Event listeners
 window.addEventListener('resize', closeMenusOnResize, false);
 toggle.addEventListener('click', toggleMenu, false);
